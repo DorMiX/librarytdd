8c42578182282b6f87612ae6125d9bd79eb890fc
DorMiX <dormany.robert@gmail.com>
2020-11-30 22:25:31 +0100

    end of episode 1

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -9,23 +9,24 @@ use App\Models\Book;
 class BooksController extends Controller
 {
     //
-    public function store() {
-
-        $data = request()->validate([
-            'title' => 'required',
-            'author' => 'required',
-        ]);
-
-        Book::create($data);
+    public function store()
+    {
+        Book::create($this->validateRequest());
     }
 
     public function update(Book $book)
     {
-        $data = request()->validate([
+        $book->update($this->validateRequest());
+    }
+
+    /**
+     * @return mixed
+     */
+    public function validateRequest()
+    {
+        return request()->validate([
             'title' => 'required',
             'author' => 'required',
         ]);
-
-        $book->update($data);
     }
 }
