9149d17ca428967d3bac7dc4b8b538c673ba5f09
DorMiX <dormany.robert@gmail.com>
2020-12-04 20:11:54 +0100

    extended path() function is working

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -9,8 +9,8 @@ use App\Models\Book;
 class BookReservationTest extends TestCase
 {
     use RefreshDatabase;
-    /** @test */
     // public function test_a_book_can_be_added_to_the_library_test() // phpunit
+    /** @test */
     public function ABookCanBeAddedToTheLibrary() // Laravel
     {
         $this->withoutExceptionHandling();
@@ -25,7 +25,7 @@ class BookReservationTest extends TestCase
         // $response->assertOK();
         $this->assertCount(1, Book::all());
 
-        $response->assertRedirect('/books' . $book->id);
+        $response->assertRedirect($book->path());
     }
     /** @test */
     public function ATitleIsRequired()
@@ -63,14 +63,14 @@ class BookReservationTest extends TestCase
 
         $book = Book::first();
 
-        $response = $this->patch('/books/' . $book->id, [
+        $response = $this->patch($book->path(), [
           'title' => 'New Title',
           'author' => 'New Author'
         ]);
 
         $this->assertEquals('New Title', Book::first()->title);
         $this->assertEquals('New Author', Book::first()->author);
-        $response->assertRedirect('/books' . $book->id);
+        $response->assertRedirect($book->fresh()->path());
 
     }
 
@@ -87,7 +87,7 @@ class BookReservationTest extends TestCase
         $book = Book::first();
         $this->assertCount(1, Book::all());
 
-        $response = $this->delete('/books/' . $book->id);
+        $response = $this->delete($book->path());
 
         $this->assertCount(0, Book::all());
         $response->assertRedirect('/books');
