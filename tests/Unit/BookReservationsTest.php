b5ef2d42f9b86838f2fcd0bb0f52984e3378a971
DorMiX <dormany.robert@gmail.com>
2020-12-20 10:30:25 +0100

    BookReservationTest is completed, End of Episode 4

--- tests/Unit/BookReservationsTest.php
+++ tests/Unit/BookReservationsTest.php
@@ -43,4 +43,42 @@ class BookReservationsTest extends TestCase
         $this->assertNotNull(Reservation::first()->checked_in_at);
         $this->assertEquals(now(), Reservation::first()->checked_in_at);
     }
+
+    /** @test */
+    public function IfNotCheckedOutExceptionIsThrown()
+    {
+        $this->expectException(\Exception::class);
+
+        $book = Book::factory()->create();
+        $user = User::factory()->create();
+
+        $book->checkin($user);
+    }
+
+    /** @test */
+    public function AUserCanCheckOutABookTwice()
+    {
+        $book = Book::factory()->create();
+        $user = User::factory()->create();
+
+        $book->checkout($user);
+
+        $book->checkin($user);
+
+        $book->checkout($user);
+
+        $this->assertCount(2, Reservation::all());
+        $this->assertEquals($user->id, Reservation::find(2)->user_id);
+        $this->assertEquals($book->id, Reservation::find(2)->book_id);
+        $this->assertNull(Reservation::find(2)->checked_in_at);
+        $this->assertEquals(now(), Reservation::find(2)->checked_out_at);
+
+        $book->checkin($user);
+
+        $this->assertCount(2, Reservation::all());
+        $this->assertEquals($user->id, Reservation::find(2)->user_id);
+        $this->assertEquals($book->id, Reservation::find(2)->book_id);
+        $this->assertNotNull(Reservation::find(2)->checked_in_at);
+        $this->assertEquals(now(), Reservation::find(2)->checked_in_at);
+    }
 }
