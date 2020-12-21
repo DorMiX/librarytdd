0ce5adc9dd3775e20e0c29240a1908edf2c46989
DorMiX <dormany.robert@gmail.com>
2020-12-21 23:16:05 +0100

    OnlySignedInUserCanCheckinABook works

--- app/Http/Controllers/CheckinBookController.php
+++ app/Http/Controllers/CheckinBookController.php
@@ -8,6 +8,11 @@ use App\Models\Book;
 class CheckinBookController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(Book $book)
     {
         $book->checkin(auth()->user());
--- tests/Feature/BookCheckoutTest.php
+++ tests/Feature/BookCheckoutTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Book;
@@ -89,5 +90,46 @@ class BookCheckoutTest extends TestCase
         $this->assertEquals(now(), Reservation::first()->checked_out_at);
         $this->assertEquals(now(), Reservation::first()->checked_in_at);
     }
+
+
+    /** @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function OnlySignedInUserCanCheckinABook()
+    {
+        // $this->withoutExceptionHandling();
+
+        $book = Book::factory()->create();
+
+        $this->actingAs(User::factory()->create())
+            ->post('/checkout/'. $book->id);
+
+
+        Auth::logout();
+
+
+        $this->post('/checkin/'. $book->id)->assertRedirect('/login');
+
+        $this->assertCount(1, Reservation::all());
+        $this->assertNull(Reservation::first()->checked_in_at);
+    }
+
+    /** @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    // public function OnlyRealBooksCanBeCheckedOut()
+    // {
+    //     // $this->withoutExceptionHandling();
+    //
+    //     $this->actingAs($user = User::factory()->create())
+    //         ->post('/checkout/9')
+    //         ->assertStatus(404);
+    //
+    //     $this->assertCount(0, Reservation::all());
+    // }
 
 }
