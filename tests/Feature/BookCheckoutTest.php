9dd4f5b3a90dbf2813ef1c72489bff25d0562d26
DorMiX <dormany.robert@gmail.com>
2020-12-21 21:32:44 +0100

    ABookCanBeCheckedInByASignedUser works

--- tests/Feature/BookCheckoutTest.php
+++ tests/Feature/BookCheckoutTest.php
@@ -58,10 +58,36 @@ class BookCheckoutTest extends TestCase
     {
         // $this->withoutExceptionHandling();
 
-$this->actingAs($user = User::factory()->create())
+        $this->actingAs($user = User::factory()->create())
             ->post('/checkout/9')
             ->assertStatus(404);
 
         $this->assertCount(0, Reservation::all());
     }
+
+    /** @test
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function ABookCanBeCheckedInByASignedUser()
+    {
+        $this->withoutExceptionHandling();
+
+        $book = Book::factory()->create();
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->post('/checkout/'. $book->id);
+
+        $this->actingAs($user)
+            ->post('/checkin/'. $book->id);
+
+        $this->assertCount(1, Reservation::all());
+        $this->assertEquals($user->id, Reservation::first()->user_id);
+        $this->assertEquals($book->id, Reservation::first()->book_id);
+        $this->assertEquals(now(), Reservation::first()->checked_out_at);
+        $this->assertEquals(now(), Reservation::first()->checked_in_at);
+    }
+
 }
