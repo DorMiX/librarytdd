9ea478492e31fa96dc510fa32d79617780d49134
DorMiX <dormany.robert@gmail.com>
2020-12-28 20:55:10 +0100

    TailwindCSS is installed 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
+ // .sass('resources/sass/app.scss', 'public/css')
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
     .postCss("resources/css/app.css", "public/css", [
      require("tailwindcss"),
     ]);
