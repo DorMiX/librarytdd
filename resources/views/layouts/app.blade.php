244233ee037057f34c840c3501ee2112080fc3ba
DorMiX <dormany.robert@gmail.com>
2021-01-02 00:26:42 +0100

    dusk_testing environment works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <h1> {{ env('APP_ENV') }} </h1>
     <div id="app">
         {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
